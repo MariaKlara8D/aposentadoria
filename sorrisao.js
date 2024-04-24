7d1f33de8613d68e81b6cdeaab0dd968d8fc8f69
MariaKlara8D <159533197+MariaKlara8D@users.noreply.github.com>
2024-04-24 15:33:35 -0300

    kkkk
    kkkk

---
+++ sorrisao.js
@@ -0,0 +1,16 @@
+const nome= "guilherme";
+const sexo = "M";
+const idade = 57;
+const contribuição = 38;
+
+const idade_e_contribuiçao = (idade+contribuição)
+
+if(contribuição>= 35){
+    if(idade_e_contribuiçao>=95){
+        console.log(`Parabéns ${nome},você conseguise aposentar`)
+    }else{
+        console.log(`${nome}, voce não cumpre blabla bla`)
+    }
+}else{
+    console.log("Quasela, vcapenas cumpre...")
+}
